49cafd7745306e7a6a400f7800c5b43d1905a49c
Frank Chambergo <franky4908@gmail.com>
2022-12-12 02:14:29 -0500

    comments

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -7,11 +7,13 @@ import { AlgoliaApi } from '../shared/algolia_api.service';
   styleUrls: ['./history.component.css']
 })
 export class HistoryComponent implements OnInit {
+  //Array of previous searches
   public history: string[] = [];
 
   constructor(private service: AlgoliaApi) { }
 
   ngOnInit(): void {
+    //On initial component load, get the previous searches from the share file that tracks previous searches
     this.history = this.service.getHistory();
   }
 
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -9,10 +9,12 @@ import { AlgoliaApi } from '../shared/algolia_api.service';
 })
 export class SearchComponent implements OnInit {
 
+  //Initialize search form. Was going to put in other query parameters but none specificed
   searchForm = new FormGroup({
     query: new FormControl("", Validators.required)
   })
 
+  //Results array for response from server. Would make a model/class for this
   results: any[] = [];
 
 
@@ -22,10 +24,14 @@ export class SearchComponent implements OnInit {
 
   }
 
+  //Form submission handler
   onSubmit() {
+    //Verify that query is given a value
     if (this.searchForm.valid) {
+      //Get user given query
       const query = this.searchForm.get("query")?.value;
 
+      //Make api call using provided query and set results locally
       this.service.searchQuery(query as string).subscribe(res => { 
         this.results = res;
       })
