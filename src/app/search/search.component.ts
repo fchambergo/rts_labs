86b22f4497b83ad1bba11117637570cbb38e7a5e
Frank Chambergo <franky4908@gmail.com>
2022-12-11 22:34:12 -0500

    initial form for query

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
+  searchForm = new FormGroup({
+    query: new FormControl("", Validators.required),
+    //tags : new FormControl(""),
+    //numericFilters: new FormControl("")
+    //page: new FormControl("")
+  })
+
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+  onSubmit() {
+    const query = this.searchForm.get("query")?.value;
+
+    console.log(query);
+  }
 }
